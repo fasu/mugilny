8d7293cee6b05671225bf2d56f4e3803fa9134a3
Teppo Lindell <teppo.lindell@gmail.com>
2016-05-11 23:01:40 +0300

    indexin tarjoilu väkisin pois, niin voi lukea jsonia

--- server/routes.js
+++ server/routes.js
@@ -8,8 +8,5 @@ module.exports = function (app) {
   app.route('/:url(app|components|bower_components)/*')
    .get(errors[404]);
 
-  app.route('/*')
-    .get(function (req, res) {
-      res.sendFile(path.join(app.get('appPath'), 'index.html'));
-    });
+
 };
