e293ffea24fbccb1c837d292858f4629ab6975c2
Teppo Lindell <teppo.lindell@gmail.com>
2016-05-17 10:02:55 +0300

    todoks geoip fallback, jos ei geolocationia saada

---
+++ README.md
@@ -0,0 +1,11 @@
+# Mugilny
+
+Näyttää käyttäjän läheltä viimeisimmät Untappd checkinit. Tai näyttää ainakin tulevaisuudessa, jos Untappdilta heltiää API access.
+
+## TODO
+
+* Testit
+* HTTPS-tuki, niin geolocation toimii uudemmilla Chromeillakin.
+* Untappd apin tuki
+* Popuppiin lisätietoja esim. Venuesta parhaat oluet, etäisyys, linkki reittioppaaseen yms.
+* Autentikoidulle Untappd-käyttäjälle hakuehtojen määrittelyä
--- client/app/main/kartta-controller.js
+++ client/app/main/kartta-controller.js
@@ -15,10 +15,9 @@ karttaController.controller("KarttaController", [ '$scope', 'Mugit', '_', '$geol
     $scope.vectorit["searchRadius"] = circlePath(position.coords.latitude, position.coords.longitude);
     $scope.vectorit["userPosition"] = circleMarkerPath(position.coords.latitude, position.coords.longitude);
     $scope.mapCenter = { lat: position.coords.latitude, lng: position.coords.longitude, zoom: 13 };
-  },
-    function(error) {
-      console.log("ei geolocationia");
-    });
+  }, function(error) {
+        // GeoIP?
+  });
 
   Mugit.viimeisimmat().success(function(data) {
     _.forEach(data.checkins, function(checkin) {
